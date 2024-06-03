d812beaf4e8d9c3b714197f552ff9df3890e7695
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-06-03 22:43:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -38,13 +38,13 @@ function App() {
 
   return (
     <div className="w-full max-w-md mx-auto shadow-md 
-    rounded-lg px-4 py-3 my-8 bg-gray-800
-    text-orange-500 ">
+    rounded-lg px-6 py-4 my-10 bg-pink-400
+    text-black-500 ">
       <h1 classname="text-white text-center
       my-3">Password Generator</h1>
     
     <div className="flex shadow rounded-lg
-    overflow-hidden mb-4">
+    overflow-hidden mb-10">
       <input 
         type="text"
         value={password}
